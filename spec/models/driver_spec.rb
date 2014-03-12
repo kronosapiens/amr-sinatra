f772b33d0524b6e28a0a257cfc65121a207a3be4
Daniel Kronovet <kronovet@gmail.com>
2014-03-12 13:08:24 -0400

    fixed a glitch in the driver spec

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -2,26 +2,28 @@ require 'spec_helper'
 
 describe "Driver" do
 
-  let(:driver){ Driver.new({:search_query => "bicycle", :min_price => 100}) }
+  let(:driver){ Driver.new({"search_query" => "bicycle", "min_price" => 100}) }
 
   it 'accepts a search query' do
     expect(driver.query_hash).to be_a(Hash)
   end
 
-  before do
-    driver.prepare_search
-  end
+  describe 'preparing a search' do
+    before do
+      driver.prepare_search
+    end
 
-  it 'created a query object' do
-    expect(driver.query).to be_a(Query)
-  end
+    it 'created a query object' do
+      expect(driver.query).to be_a(Query)
+    end
 
-  it 'created a scraper object' do
-    expect(driver.scraper).to be_a(Scraper)
-  end
+    it 'created a scraper object' do
+      expect(driver.scraper).to be_a(Scraper)
+    end
 
-  it 'knows the right query' do
-    expect(driver.query.search_query).to eq("bicycle")
+    it 'knows the right query' do
+      expect(driver.query.search_query).to eq("bicycle")
+    end
   end
 
 end
