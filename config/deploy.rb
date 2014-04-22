dd4ff862c8226a406256a7164670c94b92ce4e88
Daniel Kronovet <kronovet@gmail.com>
2014-04-22 01:33:17 -0400

    added capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require 'bundler/capistrano'
+
+set :application, "amr_sinatra"
+set :repository,  "amr_sinatra"
+
+set :user, "kronosapiens"
+set :deploy_to, "/home/kronsapiens/amr_sinatra"
+set :use_sudo, false
+
+ set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+default_run_options[:pty] = true
+
+role :web, "162.243.45.104"                          # Your HTTP server, Apache/etc
+role :app, "162.243.45.104"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
